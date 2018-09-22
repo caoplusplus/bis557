31e153de7336406fec53e8b77abceb94210485c6
caoyue <caoyue@YdeMacBook-Pro.local>
2018-09-22 15:55:29 -0400

    Linear model

--- R/linear-models.r
+++ R/linear-models.r
@@ -10,5 +10,18 @@
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  # Your code here.
+  mf <- match.call(expand.dots = FALSE)
+  m <- match(c("formula", "data"), names(mf), 0L)
+  mf <- mf[c(1L, m)]
+  mf$drop.unused.levels <- TRUE
+  mf[[1L]] <- as.name("model.frame")
+  mf <- eval(mf, parent.frame())
+  y <- model.response(mf, "numeric")
+  x <- model.matrix(formula, data)
+  udv <- svd(x)
+  x.inv <- udv$v %*% diag(1 / udv$d) %*% t(udv$u)
+  pseudo.inv <- x.inv %*% y
+  attr(pseudo.inv, "names") <- colnames(x)
+  x.pseudo.inv <- list(coefficients = pseudo.inv)
+  x.pseudo.inv
 }
