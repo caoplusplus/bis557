1e03df23039b66a062b2426adfe4ccee7f3bbe22
caoyue <yue.cao@yale.edu>
2018-10-13 14:49:52 -0400

    Another day, another update.

---
+++ R/data-ridge-test.r
@@ -0,0 +1,10 @@
+
+#' A test set for the second homework
+#'
+#' @name ridge_test
+#' @docType data
+#' @rdname ridge_test
+#' @description In homework 2 we also look at the effect on the out-of-sample
+#' mean square error as λ varies in ridge regressions. This data set gives us
+#' a basis for exploration.
+NULL
---
+++ R/data-ridge-train.r
@@ -0,0 +1,10 @@
+
+#' A training set for the second homework
+#'
+#' @name ridge_train
+#' @docType data
+#' @rdname ridge_train
+#' @description In homework 2 we explore the effect of numerical instability
+#' on ridge regressions. This data set gives us a basis for exploration.
+NULL
+
