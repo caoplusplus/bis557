588cb65bc4c315a26d1d57cbd007174c6f6138a9
caoyue <yue.cao@yale.edu>
2018-10-13 14:40:40 -0400

    first commit

---
+++ R/ridge-reg.r
@@ -0,0 +1,29 @@
+#' Fit a ridge regression model
+#'
+#' @description This function passes parameters to coefficients in ridge regression.
+#' @param formula a formula
+#' @param lambda a ridge regression parameter
+#' @param data a data.frame
+#' @return A ridge regression object
+#' @examples
+#' fit_ridge <- ridge_reg(y ~. -1, lambda, ridge_train_scale)
+#' summary(fit_ridge)
+#' @export
+ridge_reg <- function(formula, lambda, data) {
+  rownames(data) <- NULL
+  x <- model.matrix(formula, data)
+  y <- as.matrix(data[, as.character(formula)[2]], ncol = 1)
+  y <- y[as.numeric(rownames(x)),, drop = FALSE]
+  
+  svd_obj <- svd(x)
+  U <- svd_obj$u
+  V <- svd_obj$v
+  svals <- svd_obj$d
+  D <- diag(svals / (svals^2 + lambda))
+  beta <- V %*% D %*% t(U) %*% y
+  
+  rownames(beta) <- colnames(x)
+  ret <- list(coefficients = beta, lambda = lambda)
+  class(ret) <- "ridge_reg"
+  ret
+}
